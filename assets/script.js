3882200ad13ea32702bab90ff8586adce1d92942
Liam Bradley <liamhbradley11@gmail.com>
2020-09-22 21:20:01 -0700

    updated questions layout

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,48 @@
 var questions = [
     
-        newQuestion: ("What was Ariana Grande's third studio album titled?",
-        ["Dangerous Woman", "Yours Truly", "Sweetener", "Thank U, Next"], "Dangerous Woman"),
-        newQuestion: ( "What year was Ariana Grande born?",
-        ["1994","1990","1996","1993"], "1993"),
-        newQuestion: ("What is Ariana Grande's net worth?",
-        ["75 million","100 million","68 million","20 million"], "100 million"),
-        newQuestion: ("Finish the lyric.  Look at my _____, look at my _____. Ain't got enough money to pay me respect.",
-        ["neck, check","neck, jet","jet,check","net, jet"],"neck, jet")
+        {questions: "What was Ariana Grande's third studio album titled?",
+
+        A:"Dangerous Woman", 
+        B:"Yours Truly", 
+        C:"Sweetener", 
+        D:"Thank U, Next", 
+        correct: "Dangerous Woman"
+    },
+
+        {questions:"What year was Ariana Grande born?",
+
+        A:"1994",
+        B:"1990",
+        C:"1996",
+        D:"1993", 
+        correct: "1993"
+    },
+
+        {questions: "What is Ariana Grande's net worth?",
+
+        A:"75 million", 
+        B:"100 million", 
+        C:"68 million", 
+        D:"20 million", 
+        correct: "100 million"
+    },
+
+        {questions: "Finish the lyric.  Look at my _____, look at my _____. Ain't got enough money to pay me respect.",
+
+        A:"neck, check",
+         B:"neck, jet", 
+         C:"jet,check", 
+         D:"net, jet", 
+         correcct: "neck, jet"
+        }
     ];
+
+var beginPage = document.getElementById("begin");
+var beginButton = document.getElementById("begin-btn")
+
+var qPage = document.getElementById("question-pg");
+var question = document.getElementById("questions");
+var buttonA = document.getElementById("optionA");
+var buttonB = document.getElementById("optionB");
+var buttonC = document.getElementById("optionC");
+var buttonD = document.getElementById("optionD");
