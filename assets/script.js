ee14e5763f25ed550ec21346b18da31fb1d20a04
Liam Bradley <liamhbradley11@gmail.com>
2020-09-22 20:58:50 -0700

    created a card

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var questions = [
+    
+        newQuestion: ("What was Ariana Grande's third studio album titled?",
+        ["Dangerous Woman", "Yours Truly", "Sweetener", "Thank U, Next"], "Dangerous Woman"),
+        newQuestion: ( "What year was Ariana Grande born?",
+        ["1994","1990","1996","1993"], "1993"),
+        newQuestion: ("What is Ariana Grande's net worth?",
+        ["75 million","100 million","68 million","20 million"], "100 million"),
+        newQuestion: ("Finish the lyric.  Look at my _____, look at my _____. Ain't got enough money to pay me respect.",
+        ["neck, check","neck, jet","jet,check","net, jet"],"neck, jet")
+    ];
